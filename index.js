0a5593936c2238078b2db402ec5dcd0ad20c0fc2
Andrew Muto <amuto77@gmail.com>
2020-05-21 15:07:58 -0400

    corrections

--- index.js
+++ index.js
@@ -106,8 +106,7 @@ console.log(getWinnersByYear(getWinners,getYears));
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
 function getAverageGoals(data) {
-
-    /* code here */
+const homeTeamAvg = fifaData 
 
 };
 
